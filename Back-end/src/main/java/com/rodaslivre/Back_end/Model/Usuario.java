dd31f4ad7de7b7a0ff3e3d813484734c24b6fca1
Ygorgen <githubygoraraujo@gmail.com>
2024-09-18 17:33:04 -0300

    implementacao de permissoes e roles

--- Back-end/src/main/java/com/rodaslivre/Back_end/Model/Usuario.java
+++ Back-end/src/main/java/com/rodaslivre/Back_end/Model/Usuario.java
@@ -1,32 +1,42 @@
 package com.rodaslivre.Back_end.Model;
 
 import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "tb_user")
 @Data
-public class Usuario{
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotBlank
-    @Size(min = 2 , max = 100)
+    @Size(min = 2, max = 100)
     private String nome;
 
     @NotBlank
-    @Size(min = 11 , max = 11)
+    @Size(min = 11, max = 11)
     private String cpf;
 
     @NotBlank
@@ -36,12 +46,27 @@ public class Usuario{
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataEnvioCodigo;
-    
+
     private String senha;
+
+    @OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = { CascadeType.PERSIST,
+            CascadeType.MERGE }, fetch = FetchType.EAGER)
+    @Setter(value = AccessLevel.NONE)
+    private List<UserPermission> userPermission;
+
+    @OneToOne(mappedBy = "usuario")
+    @JsonIgnoreProperties("usuario")
+    private Reserva reserva;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
 
+    public void setUserPermission(List<UserPermission> up) {
+        for (UserPermission u : up) {
+            u.setUsuario(this);
+        }
+        this.userPermission = up;
+    }
 }
